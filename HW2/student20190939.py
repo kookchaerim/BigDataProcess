23e2ec27406b34b6af76c9dbf85e827ab559c38e
kookchaerim <kooc626@naver.com>
2022-10-17 19:31:29 -0700

    add2

--- HW2/student20190939.py
+++ HW2/student20190939.py
@@ -5,6 +5,7 @@ import openpyxl
 wb = openpyxl.load_workbook( "student.xlsx" )
 ws = wb['Sheet1']
 
+list=[]
 row_id = 1;
 for row in ws:
 	if row_id != 1:
@@ -13,32 +14,23 @@ for row in ws:
 		sum_v += ws.cell(row = row_id, column = 5).value * 0.34
 		sum_v += ws.cell(row = row_id, column = 6).value
 		ws.cell(row = row_id, column = 7).value = sum_v
+		list.append(sum_v)	
 	row_id += 1
 
-gradeList=[];
 row_id = 1;
+result = []
 
-for row in ws:
-	if row_id != 1:
-		gradeList.append(ws.cell(row = row_id, column = 7).value)
-	row_id += 1
-	
-
-result=[];
-row_id = 0;
-
-for i in gradeList:
+for i in list:
 	rank = 1
-	for j in gradeList:
+	for j in list:
 		if i < j:
 			rank += 1
 	result.append(rank)
 
-
 row_id = 2;
 tmp = len(result) / 10
 for i in result:
-	if i == (tmp*3 / 2):
+	if i <= (tmp*3 / 2):
 		ws.cell(row = row_id, column = 8).value = 'A+'
 	elif i <= tmp*3:
 		ws.cell(row = row_id, column = 8).value = 'A0'
@@ -51,5 +43,4 @@ for i in result:
 	else:
 		ws.cell(row = row_id, column = 8).value = 'C0'
 	row_id += 1
-
 wb.save("student.xlsx")
